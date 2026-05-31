87ca6cdaa268cc2ca3379b8429c2c24379c4dc31
Pedro Lemes <113155395+code-pedrolsl@users.noreply.github.com>
2026-05-31 12:19:24 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,7 @@
-# IPs das suas instâncias AWS (substitua pelos IPs reais)
-MACHINE_A = "3.227.138.6"   # producer / server / publisher     # server
-MACHINE_B = "3.235.239.109"   # middle / client / subscriber    # peer1
-MACHINE_C = "100.52.205.229"   # consumer final                 # peer2
+MACHINE_A = "44.202.179.145"   # producer / server / publisher     # server
+MACHINE_B = "98.84.55.114"   # middle / client / subscriber    # peer1
+MACHINE_C = "34.205.26.151"   # consumer final                 # peer2
 
-PORT_1 = "5678"   # A → B  (client-server, pub-sub, pipeline etapa 1)
-PORT_2 = "5679"   # B → C  (pipeline etapa 2)
-PORT_3 = "5680"   # reserva
+PORT_1 = "5678"
+PORT_2 = "5679"
+PORT_3 = "5680"
